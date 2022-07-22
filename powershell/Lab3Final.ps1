11f8d0bc2fee3160351d56905c309f1b46def422
Jashnpreet <105706769+Jashnpreet@users.noreply.github.com>
2022-07-22 16:45:37 -0400

    lab3done

---
+++ powershell/Lab3Final.ps1
@@ -0,0 +1,4 @@
+﻿get-ciminstance win32_networkadapterconfiguration |
+where { $_.ipenabled -eq "true" } |
+Format-Table -AutoSize Description, Index,
+IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
